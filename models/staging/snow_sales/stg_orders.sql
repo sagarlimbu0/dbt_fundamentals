38c81ecdcfe08b1e563ad2cddea171cdd8a4fb7f
sagar limbu <sagarlimbu8525@gmail.com>
2025-04-21 04:40:21 +0000

    dbt test cases

--- models/staging/snow_sales/stg_orders.sql
+++ models/staging/snow_sales/stg_orders.sql
@@ -4,9 +4,6 @@ SELECT
         o_orderstatus, 
         o_totalprice, 
         o_orderpriority
-<<<<<<< HEAD
+
 FROM {{ source('snow_sales', 'orders') }}
 -- FROM DBT_TRIAL.DBO_DBT.orders
-=======
-FROM DBT_TRIAL.DBO_DBT.orders
->>>>>>> 9c4e1b08f702f55390406b4c1e68a8ac40764cec
