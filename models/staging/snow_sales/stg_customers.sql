67d7635ab8c93c4a0c382a4cc18e205564418e37
sagar limbu <60721866+sagarlimbu0@users.noreply.github.com>
2025-04-20 22:21:51 -0700

    01 first model (#4)
    * First Branch for the tutorial
    
    * Refactored the dim table. Add configuration on 'yml' file to materialized as TABLE and VIEW
    
    * Build Source configuration 'yml' file. Models refernced with source macro to get table records.
    
    * dbt test cases
    
    ---------
    
    Co-authored-by: sagar limbu <sagarlimbu8525@gmail.com>

--- models/staging/snow_sales/stg_customers.sql
+++ models/staging/snow_sales/stg_customers.sql
@@ -1,13 +1,9 @@
 SELECT 
-<<<<<<< HEAD
     c_custkey, 
     c_name, 
     c_address, 
-    c_nationkey, 
+    c_nationkey,
+    c_mktsegment,
     c_comment    
 FROM {{ source('snow_sales', 'customers')}}
 -- FROM DBT_TRIAL.DBO_DBT.CUSTOMER
-=======
-    c_custkey, c_name, c_address, c_nationkey, c_comment    
-FROM DBT_TRIAL.DBO_DBT.CUSTOMER
->>>>>>> 9c4e1b08f702f55390406b4c1e68a8ac40764cec
